c20773ef422c1bc48e1facd0299e630e6218db57
Dhruv Shah <42727015+Dhruv-m-Shah@users.noreply.github.com>
2018-12-17 20:05:50 -0500

    Update and rename DMOJ/IOI_2001_P1_Mobile_Phones.cpp to DMOJ/IOI_2001_P1_Mobile_Phones/IOI_2001_P1_Mobile_Phones.cpp

--- DMOJ/IOI_2001_P1_Mobile_Phones/IOI_2001_P1_Mobile_Phones.cpp
+++ DMOJ/IOI_2001_P1_Mobile_Phones/IOI_2001_P1_Mobile_Phones.cpp
@@ -28,7 +28,6 @@ int query(int x, int y)
 
 }
 
-// lesson learned, watch out for what variables I am using for query, debug time around 2 hours... Also for loop works for BIT while loop not needed.
 int main()
 {
     cin.sync_with_stdio(0);
@@ -41,17 +40,12 @@ int main()
         cin>>num1;
         if(num1 == 1)
         {
-            cin>>num2;
-            cin>>num3;
-            cin>>num4;
+            cin>>num2>>num3>>num4;
             update(num2+1, num3+1, num4);
         }
         if(num1 == 2)
         {
-            cin>>num2;
-            cin>>num3;
-            cin>>num4;
-            cin>>num5;
+            cin>>num2>>num3>>num4>>num5;
             cout<<query(num4+1, num5+1)-query(num4+1, num3)-query(num2, num5+1)+query(num2, num3)<<endl;
         }
         if(num1 == 3) break;
