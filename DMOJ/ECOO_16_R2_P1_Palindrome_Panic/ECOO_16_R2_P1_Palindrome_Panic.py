d99bf2fdefd5816d89dcd0346412cb9a6ad2d30c
Dhruv Shah <42727015+Dhruv-m-Shah@users.noreply.github.com>
2018-12-29 18:21:41 -0500

    Create ECOO_16_R2_P1_Palindrome_Panic.py

---
+++ DMOJ/ECOO_16_R2_P1_Palindrome_Panic/ECOO_16_R2_P1_Palindrome_Panic.py
@@ -0,0 +1,44 @@
+for i in range(10):
+    y = raw_input()
+    if y == y[::-1]:
+        print(0)
+        continue
+    sample = ''
+    sample = y[::-1]
+    count = 0
+    j = 0
+    k = 0
+    while True:
+        if y[j] == sample[k]:
+            count = count+1
+            j = j+1
+            k = k+1
+        elif k != 0 and sample[k] !=y [j]:
+            k = 0
+            count = 0
+        else:
+            j = j+1
+
+        if j==len(y):
+            break
+
+    sufmin = len(y) - count
+    
+    count = 0
+    j = 0
+    k = 0
+    while True:
+        if sample[j] ==  y[k]:
+            count = count+1
+            j = j+1
+            k = k+1
+        elif k != 0 and y[k] != sample[j]:
+            k = 0
+            count = 0
+        else:
+            j = j+1
+
+        if j==len(y):
+            break
+
+    print(min(len(y) - count, sufmin))
