0c898e0ea1d334a3a5c1c28eb58973ec5716e1b8
Dhruv Shah <42727015+Dhruv-m-Shah@users.noreply.github.com>
2019-12-13 23:14:57 -0500

    Create Linked List.cpp

---
+++ General Algorithms/Linked List.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+};
+
+// Traverses a linkedlist.
+// traverse: Node -> (output of integers)
+
+void traverse (Node* linkedlist)
+{
+	while(true)
+	{
+		cout<<linkedlist->data<<" ";
+		if(linkedlist->next == NULL){
+			break;
+		}
+		linkedlist = linkedlist->next;
+	}
+	cout<<""<<endl; // Starts new line.
+}
+
+
+// Reverses a linkedlist.
+// Node -> Node
+
+Node* reverse (Node* linkedlist)
+{
+	Node* prev;
+    Node* curr;
+    Node* next;
+    curr = linkedlist;
+    prev = NULL;
+	while (curr!=NULL)
+	    {
+	        next = curr->next;
+	        curr->next = prev;
+	        prev = curr;
+	        curr = next;
+	    }
+	return prev;
+}
+
+int main()
+{
+	// Creating a sample linkedlist.
+    Node* head = NULL;
+    Node* second = NULL;
+    Node* third = NULL;
+
+    head = new Node();
+    second = new Node();
+    third = new Node();
+    head->data = 5;
+    head->next = second;
+
+    second->data = 10;
+    second->next = third;
+
+    third->data = 15;
+    third->next = NULL;
+
+    traverse(reverse(head));
+    
+}
